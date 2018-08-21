7a0bffe2df8a969d51a7e643a7bd9da694903c2b
Bastien <bastien.chelle@gmail.com>
2018-08-21 17:16:32 +0200

    mise a jours de la base de données et mise en place de nouveau controlleur

--- ToDoList_WebAPI/ToDoList_WebAPI/Controllers/UserApiController.cs
+++ ToDoList_WebAPI/ToDoList_WebAPI/Controllers/UserApiController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace ToDoList_WebAPI.Controllers
 {
@@ -11,6 +10,7 @@ namespace ToDoList_WebAPI.Controllers
     public class UserApiController : ApiController
     {
         // GET: User
+        [Route("")]
         public IHttpActionResult Get()
         {
             return Ok("test");
