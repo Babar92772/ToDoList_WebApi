ba5e22fe7083cc9dac01ab59f3c7da22df4a0a93
Bastien <bastien.chelle@gmail.com>
2018-08-22 17:08:57 +0200

    Ajoute du speudo user et rendu unique de speudo et mail

--- DB/Remplir les tables.sql
+++ DB/Remplir les tables.sql
@@ -1,8 +1,8 @@
-INSERT Users( Mail, Pwd)
-VALUES('Utilisateur@Supprimer.com', 'suppr')
+INSERT Users(Speudo, Mail, Pwd)
+VALUES('UtilisateurSupprimer','Utilisateur@Supprimer.com', 'suppr')
 
-INSERT Users(Mail, Pwd)
-VALUES('test@test.com', '1')
+INSERT Users(Speudo, Mail, Pwd)
+VALUES('Test','test@test.com', '1')
 
 INSERT Tasks(TaskState, CreateDate, Note, IDUserCreator)
-VALUES('todo', '22/08/2018', 'Task de test',14785)
+VALUES('todo', '2018-08-22', 'Task de test',14787)
